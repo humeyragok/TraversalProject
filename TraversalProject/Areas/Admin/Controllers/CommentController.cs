2b3884fa9be763060876ef2cba0ba043021cb977
Hümeyra gök <80422380+humeyragok@users.noreply.github.com>
2023-08-25 15:59:48 +0300

    düzenleme yapıldı

---
+++ TraversalProject/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+        public IActionResult Index()
+        {
+            var values = commentManager.TGetList();
+            return View(values);
+        }
+    }
+}
