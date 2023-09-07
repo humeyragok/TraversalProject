5b6b56250cc9a30970e0a7e73feeb1e912220951
Hümeyra gök <80422380+humeyragok@users.noreply.github.com>
2023-09-07 13:17:13 +0300

    Rol atama işlemleri tamamlandı.

--- TraversalProject/Startup.cs
+++ TraversalProject/Startup.cs
@@ -64,6 +64,10 @@ namespace TraversalProject
 
             });
             services.AddMvc();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Login/SignIn/";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
